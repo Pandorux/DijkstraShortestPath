bb8d1eb5a56c64eb989a2c0ad545755ae1c22b3f
n <n>
2019-03-04 19:40:00 +1000

    Node Class Created, Gizmo working

---
+++ DijkstaShortestPath/Assets/Scenes/Node.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [Tooltip("What nodes is this node connected to")]
+    public Node[] neighbors;
+
+
+
+    private void OnDrawGizmos()
+    {
+        foreach (Node n in neighbors)
+        {
+            Gizmos.DrawLine(transform.position, n.transform.position);
+        }
+    }
+
+}
